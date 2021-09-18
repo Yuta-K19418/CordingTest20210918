3765448cdd6cf26dafb3b93eb48c02d7b0e08483
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-09-18 15:00:43 +0900

    設問3まで

--- CordingTest20210918/Program.cs
+++ CordingTest20210918/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace CordingTest20210918
@@ -12,8 +13,20 @@ namespace CordingTest20210918
             int timeOutCount = Int32.Parse(args[1]);
             int lastNumberOfTimes = Int32.Parse(args[2]);
             int reactionTime = Int32.Parse(args[3]);
-            string output = OutputPeriodOfBrokenServer(args[0], timeOutCount, lastNumberOfTimes, reactionTime);
+            string output = OutputPeriodOfBrokenServer(args[0],
+                                                        timeOutCount,
+                                                        lastNumberOfTimes,
+                                                        reactionTime);
             Console.WriteLine(output);
+        }
+
+        private class LogInfo
+        {
+            public DateTime LogTime { get; set; }
+
+            public string ServerAddress { get; set; }
+
+            public int? ReactionMiliSecond { get; set; }
         }
 
         public static string OutputPeriodOfBrokenServer(string inputFilePath,
@@ -26,7 +39,7 @@ namespace CordingTest20210918
             var endTimeDic = new Dictionary<string, DateTime>();
             var brokenServerDic = new Dictionary<string, TimeSpan>();
             var pingCountDic = new Dictionary<string, int>();
-            var readPinglist = new List<string>();
+            var readPinglist = new List<LogInfo>();
             string format = "yyyyMMddHHmmss";
             int number;
             var output = new StringBuilder();
@@ -36,7 +49,14 @@ namespace CordingTest20210918
                 string line = sr.ReadLine();
                 string[] values = line.Split(',');
 
-
+                // サーバーアドレスごとの件数が直近m回分以上の場合、超えた分の一番古いデータを削除
+                if (readPinglist.Where(x => x.ServerAddress == values[1])
+                                        .Select(x => x)
+                                        .Count() >= lastNumberOfTimes)
+                {
+                    var element = readPinglist.Where(x => x.ServerAddress == values[1]).FirstOrDefault();
+                    readPinglist.Remove(element);
+                }
 
                 // タイムアウトの場合
                 if (values[2] == "-")
@@ -52,6 +72,13 @@ namespace CordingTest20210918
                         pingCountDic.Add(values[1], 1);
                         startTimeDic.Add(values[1], DateTime.ParseExact(values[0], format, null));
                     }
+
+                    readPinglist.Add(new LogInfo()
+                    {
+                        LogTime = DateTime.ParseExact(values[0], format, null),
+                        ServerAddress = values[1],
+                        ReactionMiliSecond = null
+                    });
                 }
                 // 応答がある場合
                 else if (int.TryParse(values[2], out number))
@@ -68,12 +95,40 @@ namespace CordingTest20210918
                         pingCountDic.Remove(values[1]);
                         startTimeDic.Remove(values[1]);
                     }
+
+                    readPinglist.Add(new LogInfo()
+                    {
+                        LogTime = DateTime.ParseExact(values[0], format, null),
+                        ServerAddress = values[1],
+                        ReactionMiliSecond = int.Parse(values[2])
+                    });
                 }
             }
 
             foreach (var pair in brokenServerDic)
             {
                 output.Append($"故障状態のサーバアドレス：{pair.Key}, 故障期間：{pair.Value.ToString()}\r\n");
+            }
+
+            var calcuratedServerAddressList = new List<string>();
+
+            foreach (var item in readPinglist)
+            {
+                if (!calcuratedServerAddressList.Contains(item.ServerAddress))
+                {
+                    int calcuratedMiliSecond = readPinglist.Where(x => x.ServerAddress == item.ServerAddress)
+                                                                            .Sum(x => x?.ReactionMiliSecond)
+                                         / readPinglist.Where(x => x.ServerAddress == item.ServerAddress).Count() ?? 0;
+
+                    if (calcuratedMiliSecond > reactionTime)
+                    {
+                        output.Append(item.ServerAddress
+                                        + "(過負荷状態期間)："
+                                        + calcuratedMiliSecond
+                                        + "ミリ秒\r\n");
+                        calcuratedServerAddressList.Add(item.ServerAddress);
+                    }
+                }
             }
 
             return output.ToString().TrimEnd('\r', '\n');
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace UnitTest
         [Fact(DisplayName = "設問1")]
         public void Test1()
         {
-            string result = Program.OutputPeriodOfBrokenServer("./test1.txt", 1, 1, 1);
+            string result = Program.OutputPeriodOfBrokenServer("./test1.txt", 1, 1, 500);
             Assert.Equal("故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:01:00\r\n"
                                 + "故障状態のサーバアドレス：10.20.30.1/16, 故障期間：00:01:06", result);
 
@@ -25,9 +25,19 @@ namespace UnitTest
         [Fact(DisplayName = "設問2")]
         public void Test2()
         {
-            string result = Program.OutputPeriodOfBrokenServer("./test2.txt",2, 3, 1);
+            string result = Program.OutputPeriodOfBrokenServer("./test2.txt",2, 3, 5);
             Assert.Equal("故障状態のサーバアドレス：10.20.30.1/16, 故障期間：00:00:04\r\n"
                                 + "故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:00:05", result);
+
+        }
+
+        [Fact(DisplayName = "設問3")]
+        public void Test3()
+        {
+            string result = Program.OutputPeriodOfBrokenServer("./test3.txt", 2, 3, 10);
+            Assert.Equal("故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:05:00\r\n"
+                                + "192.168.1.1/24(過負荷状態期間)：11ミリ秒\r\n"
+                                + "10.20.30.1/16(過負荷状態期間)：174ミリ秒", result);
 
         }
     }
