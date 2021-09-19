4d19254d550f7311b43d811140e72b280cea1e2c
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-09-19 15:23:53 +0900

    UT修正

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace UnitTest
         [Fact(DisplayName = "設問1")]
         public void Test1()
         {
-            string result = Program.OutputPeriodOfBrokenServer("./test1.txt", 1, 1, 500);
+            string result = Program.OutputPeriodOfBrokenServer("./test1.txt", 1, 10, 500);
             Assert.Equal("故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:01:00\r\n"
                                 + "故障状態のサーバアドレス：10.20.30.1/16, 故障期間：00:01:06", result);
 
@@ -26,8 +26,8 @@ namespace UnitTest
         public void Test2()
         {
             string result = Program.OutputPeriodOfBrokenServer("./test2.txt",2, 3, 5);
-            Assert.Equal("故障状態のサーバアドレス：10.20.30.1/16, 故障期間：00:00:04\r\n"
-                                + "故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:00:05", result);
+            Assert.Equal("故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:00:05\r\n"
+                                + "故障状態のサーバアドレス：10.20.30.1/16, 故障期間：00:00:02", result);
 
         }
 
@@ -35,7 +35,7 @@ namespace UnitTest
         public void Test3()
         {
             string result = Program.OutputPeriodOfBrokenServer("./test3.txt", 2, 3, 10);
-            Assert.Equal("故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:05:00\r\n"
+            Assert.Equal("故障状態のサーバアドレス：10.20.30.2/16, 故障期間：00:04:00\r\n"
                                 + "192.168.1.1/24(過負荷状態期間)：11ミリ秒\r\n"
                                 + "10.20.30.1/16(過負荷状態期間)：174ミリ秒", result);
 
